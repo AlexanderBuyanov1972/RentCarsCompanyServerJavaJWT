49f1a1bdb296b9738417bd622a9885c8ee865789
AlexanderBuyanov1972 <AlexanderBuyanov1972@users.noreply.github.com>
2019-06-24 15:34:16 +0300

    2019/06/24

--- src/main/java/cars/security/configuration/CarsSecurityConfiguration.java
+++ src/main/java/cars/security/configuration/CarsSecurityConfiguration.java
@@ -4,28 +4,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static cars.dto.CarsApiConstants.*;
+
 @Configuration
 public class CarsSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+
         httpSecurity.httpBasic();
-//        httpSecurity.csrf().disable();
-//        httpSecurity.authorizeRequests().anyRequest().authenticated();
-         httpSecurity.authorizeRequests().anyRequest().permitAll();
-//        httpSecurity.authorizeRequests().antMatchers(SHUTDOWN, ADD_ACCOUNT, REMOVE_ACCOUNT,
-//                UPDATE_PASSWORD, ADD_ROLES, REMOVE_ROLES).hasRole("ADMIN");//+++
-//        httpSecurity.authorizeRequests().antMatchers(ADD_MODEL, ADD_CAR, REMOVE_CAR).hasRole("MANAGER");
-//        httpSecurity.authorizeRequests().antMatchers(ADD_DRIVER, RENT_CAR, RETURN_CAR).hasRole("CLERK");
-//        httpSecurity.authorizeRequests().antMatchers(GET_DRIVER_CARS, GET_CAR_DRIVERS).hasRole("DRIVER");//
-//        httpSecurity.authorizeRequests().antMatchers(MOST_POPULAR_MODELS, MOST_PROFIT_MODELS).hasRole("STATIST");//+++
-//        httpSecurity.authorizeRequests().antMatchers(GET_ALL_RECORDS).hasRole("TECHNICIAN");//+++
-//        // --------------------------------------------
-//        httpSecurity.authorizeRequests().antMatchers(GET_CAR, GET_ALL_CARS).authenticated();
-//        httpSecurity.authorizeRequests().antMatchers(GET_ALL_MODELS, GET_MODEL).permitAll();
-//        httpSecurity.authorizeRequests().antMatchers(GET_ALL_DRIVERS).hasAnyRole("DRIVER", "CLERK");
-//        httpSecurity.authorizeRequests().antMatchers(GET_DRIVER).hasAnyRole("MANAGER", "CLERK");
-//        httpSecurity.authorizeRequests().antMatchers(GET_PROFIT_MODEL).hasAnyRole("MANAGER", "STATIST");
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+
+//         httpSecurity.authorizeRequests().anyRequest().permitAll();
+
+        httpSecurity.authorizeRequests().antMatchers(SHUTDOWN, ADD_ACCOUNT, REMOVE_ACCOUNT,
+                UPDATE_PASSWORD, ADD_ROLES, REMOVE_ROLES).hasRole("ADMIN");//+++
+        httpSecurity.authorizeRequests().antMatchers(ADD_MODEL, ADD_CAR, REMOVE_CAR).hasRole("MANAGER");
+        httpSecurity.authorizeRequests().antMatchers(ADD_DRIVER, RENT_CAR, RETURN_CAR).hasRole("CLERK");
+        httpSecurity.authorizeRequests().antMatchers(GET_DRIVER_CARS, GET_CAR_DRIVERS).hasRole("DRIVER");//
+        httpSecurity.authorizeRequests().antMatchers(MOST_POPULAR_MODELS, MOST_PROFIT_MODELS).hasRole("STATIST");//+++
+        httpSecurity.authorizeRequests().antMatchers(GET_ALL_RECORDS).hasRole("TECHNICIAN");//+++
+        // --------------------------------------------
+        httpSecurity.authorizeRequests().antMatchers(GET_CAR, GET_ALL_CARS).authenticated();
+        httpSecurity.authorizeRequests().antMatchers(GET_ALL_MODELS, GET_MODEL).permitAll();
+        httpSecurity.authorizeRequests().antMatchers(GET_ALL_DRIVERS).hasAnyRole("DRIVER", "CLERK");
+        httpSecurity.authorizeRequests().antMatchers(GET_DRIVER).hasAnyRole("MANAGER", "CLERK");
+        httpSecurity.authorizeRequests().antMatchers(GET_PROFIT_MODEL).hasAnyRole("MANAGER", "STATIST");
     }
 }
 
